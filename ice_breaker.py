06e63c9e2e126af5c9b8296d4a3173ab94d2206f
Sebastian Lozano <juansebastian.lozano@publicissapient.com>
2024-01-25 17:03:49 -0500

    Added linkedin api to get data

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,30 +1,36 @@
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
+from third_parties.linkedin import scrape_linkedin_profile
+from third_parties.linkedin import scrape_linkedin_profile_with_curl_proxy
 
 information = """
-    Elon Reeve Musk (/ˈiːlɒn/; EE-lon; born June 28, 1971) is a businessman and investor. He is the founder, chairman, CEO, and CTO of SpaceX; angel investor, CEO, product architect and former chairman of Tesla, Inc.; owner, chairman and CTO of X Corp.; founder of the Boring Company and xAI; co-founder of Neuralink and OpenAI; and president of the Musk Foundation. He is the wealthiest person in the world, with an estimated net worth of US$232 billion as of December 2023, according to the Bloomberg Billionaires Index, and $254 billion according to Forbes, primarily from his ownership stakes in Tesla and SpaceX.[5][6]
+Samuel Benjamin Bankman-Fried[2] (born March 5, 1992), or SBF,[3] is an American entrepreneur who was convicted of fraud and related crimes in November 2023. Bankman-Fried founded the FTX cryptocurrency exchange and was celebrated as a "poster boy" for crypto.[4] At the peak of his success, he was ranked the 41st-richest American in the Forbes 400.[5]
 
-A member of the wealthy South African Musk family, Elon was born in Pretoria and briefly attended the University of Pretoria before immigrating to Canada at age 18, acquiring citizenship through his Canadian-born mother. Two years later, he matriculated at Queen's University at Kingston in Canada. Musk later transferred to the University of Pennsylvania, and received bachelor's degrees in economics and physics. He moved to California in 1995 to attend Stanford University. However, Musk dropped out after two days and, with his brother Kimbal, co-founded online city guide software company Zip2. The startup was acquired by Compaq for $307 million in 1999, and, that same year Musk co-founded X.com, a direct bank. X.com merged with Confinity in 2000 to form PayPal.
+The public persona of Bankman-Fried masked significant problems at FTX, and in November 2022 when evidence of potential fraud began to surface, depositors quickly withdrew their assets, forcing the company into bankruptcy. On December 12, 2022, Bankman-Fried was arrested in the Bahamas and extradited to the United States, where he was indicted on seven criminal charges including wire fraud, commodities fraud, securities fraud, money laundering, and campaign finance law violations.[6][7][8]
 
-In October 2002, eBay acquired PayPal for $1.5 billion, and that same year, with $100 million of the money he made, Musk founded SpaceX, a spaceflight services company. In 2004, he became an early investor in electric vehicle manufacturer Tesla Motors, Inc. (now Tesla, Inc.). He became its chairman and product architect, assuming the position of CEO in 2008. In 2006, Musk helped create SolarCity, a solar-energy company that was acquired by Tesla in 2016 and became Tesla Energy. In 2013, he proposed a hyperloop high-speed vactrain transportation system. In 2015, he co-founded OpenAI, a nonprofit artificial intelligence research company. The following year, Musk co-founded Neuralink—a neurotechnology company developing brain–computer interfaces—and the Boring Company, a tunnel construction company. In 2022, he acquired Twitter for $44 billion. He subsequently merged the company into newly created X Corp. and rebranded the service as X the following year. In March 2023, he founded xAI, an artificial intelligence company.
-
-Musk has expressed views that have made him a polarizing figure.[7] He has been criticized for making unscientific and misleading statements, including COVID-19 misinformation, and antisemitic conspiracy theories.[7][8][9][10] His ownership of Twitter has been similarly controversial, being marked by the laying off of a large number of employees, an increase in hate speech and misinformation and disinformation on the website, as well as changes to Twitter Blue verification. In 2018, the U.S. Securities and Exchange Commission (SEC) sued him for falsely tweeting that he had secured funding for a private takeover of Tesla. To settle the case, Musk stepped down as the chairman of Tesla and paid a $20 million fine.
+On November 2, 2023, in the case of United States v. Bankman-Fried, he was convicted of all seven counts of fraud, conspiracy, and money laundering.[9] His sentencing, where experts say he faces decades in prison,[10][11] is scheduled for March 28, 2024.[12]
 """
 
 if __name__ == '__main__':
      print('Hello Langchain')
 
-     summary_template = """
-         given the information {information} about a person from I want you to create
-         1. a short summary
-         2. two interesting facts about them
-     """
+     # summary_template = """
+     #     given the information {information} about a person from I want you to create
+     #     1. a short summary
+     #     2. two interesting facts about them
+     # """
+     #
+     # summary_prompt_template = PromptTemplate(input_variables = ["information"], template = summary_template)
+     #
+     # llm = ChatOpenAI(temperature = 0, model_name = "gpt-3.5-turbo")
+     #
+     # chain = LLMChain(llm = llm, prompt = summary_prompt_template)
+     #
+     # print(chain.run(information=information))
 
-     summary_prompt_template = PromptTemplate(input_variables = ["information"], template = summary_template)
+sebastian_data = scrape_linkedin_profile()
+print(sebastian_data.json())
 
-     llm = ChatOpenAI(temperature = 0, model_name = "gpt-3.5-turbo")
-
-     chain = LLMChain(llm = llm, prompt = summary_prompt_template)
-
-     print(chain.run(information=information))
+#linkedin_data = scrape_linkedin_profile_with_curl_proxy(linkedin_profile_url="https://www.linkedin.com/in/nvaz/")
+#print(linkedin_data)
---
+++ third_parties/linkedin.py
@@ -0,0 +1,36 @@
+##marco_response: https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/fad4d7a87e3e934ad52ba2a968bad9eb45128665/eden-marco.json
+import os
+import requests
+
+
+# gist_response = requests.get("https://gist.githubusercontent.com/jlozanop42/945f4e760631a74617dfe6e0ae715d3d/raw/cc7f99081ae69f93aadd2fe6f167ba5424daa463/sebastian-lozano.json")
+# print(gist_response.json())
+# print(gist_response.json()['full_name'])
+def scrape_linkedin_profile():
+    return requests.get(
+        "https://gist.githubusercontent.com/jlozanop42/945f4e760631a74617dfe6e0ae715d3d/raw"
+        "/c40537711d2cd502421ee8a936b16c6a5b910efc/sebastian-lozano.json")
+
+
+# To use the proxycurl linkedin feature
+# def scrape_linkedin_profile_with_curl_proxy(linkedin_profile_url: str):
+#     print(linkedin_profile_url)
+#     api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+#     header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
+#
+#     response = requests.get(
+#         api_endpoint, params={"url": linkedin_profile_url}, headers=header_dic
+#     )
+#
+#     data = response.json()
+#     data = {
+#         k: v
+#         for k, v in data.items()
+#         if v not in ([], "", "", None)
+#            and k not in ["people_also_viewed", "certifications"]
+#     }
+#     if data.get("groups"):
+#         for group_dict in data.get("groups"):
+#             group_dict.pop("profile_pic_url")
+#
+#     return data
